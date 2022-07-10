5a55ac937adb391c5f0f571a027afe3afd2673ce
Gunter <kaiz@yandex.ru>
2022-07-10 16:04:30 +0300

    seminar 4 end

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+// Console.WriteLine("input number: ");
+// int sum = Convert.ToInt32(Console.ReadLine());
+
+
+// int n = 0;
+
+// while (sum > 0)
+// {
+// int num = sum % 10;
+// sum = sum / 10;
+// n = n + num;
+// }
+// Console.WriteLine("The sum of figures is: " + n);
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int size = 8;
+// int[] array = new int[size];
+ 
+// for(int i = 0; i < size; i++)
+// {
+//     array[i] = new Random().Next(0, 200);
+//     Console.Write(array[i] + " ");
+// }
+
+
+
+
